121eeecb10aa4671a05e5de40961ff3445285a3b
julianodesanti <julianodesanti@live.com>
2023-01-10 23:32:46 -0300

    Adicionando videos fav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-function dimensaoTela(){
-    var heightTela = window.innerHeight;
-    var widthTela = window.innerWidth;
-
-    document.getElementById('pag_processos').style.minHeight = heightTela+'px';
-}
 const t = new Date().toLocaleDateString();
 const c = new Date().toLocaleTimeString();
 document.getElementById("clock").innerHTML = c;
